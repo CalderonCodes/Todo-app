02fb464f0f45056d02ddee7ceda52ba2954fc9c9
CalderonCodes <cgcalderondev@gmail.com>
2023-11-22 00:26:52 -0600

    feat: add complete task :sparkles:

--- src/Components/Tasks/Task.jsx
+++ src/Components/Tasks/Task.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import useStore from "../../taskStore";
 function Task({task}) {
 
-  
-  const { tasks, addTask, removeTask } = useStore();
+  const [isCompleted, setIsCompleted] = useState(false);
+  const { tasks, addTask, removeTask, updateTask } = useStore();
 
   const handleDelete = () => {
     removeTask(task.id)
+  };
+
+  const handleComplete = () => {
+    task.isCompleted = !task.isCompleted;
+    updateTask(task);
+    console.log(task);
   };
   
 
   return (
     <div className="w-full rounded-t-lg h-16 bg-base-200 flex justify-between  text-primary px-5 items-center border-b border-secondary">
       <div className='flex gap-5'>
-      <button className="btn btn-circle btn-xs   hover:bg-inherit hover:border-2  transition-none btn-outline"></button>
+      <button onClick={handleComplete} className={`btn btn-circle btn-xs   hover:bg-inherit hover:border-2  ${task.isCompleted === true ? 'bg-check-background border-none ' : 'bg-inherit'} transition-none btn-outline`}>
+        {task.isCompleted === true && <img src="assets/icon-check.svg" alt="" />}
+      </button>
       <p className='text-left'> {task.content}</p>
       </div>
      
--- src/taskStore.js
+++ src/taskStore.js
@@ -5,6 +5,7 @@ const useStore = create(persist((set, get) => ({
     tasks: [],
     addTask: (newTask) => set((state) => ({ tasks: [...state.tasks, newTask] })),
     removeTask: (taskID) => set((state) => ({ tasks: state.tasks.filter(task => task.id !== taskID) })),
+    updateTask: (taskID, newTask) => set((state) => ({ tasks: state.tasks.map(task => task.id === taskID ? newTask : task) })),
   }),
   {
     name: 'task-storage',
